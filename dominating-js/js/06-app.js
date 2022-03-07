fdc1d619a66fa071ac876e871833bbb354f6067e
iOznny <f000170014@gmail.com>
2022-03-06 20:03:52 -0600

    Dominating JS

--- dominating-js/js/06-app.js
+++ dominating-js/js/06-app.js
@@ -3,3 +3,18 @@
  * 
 */
 
+function Vehicle(model, color) {
+    this.model = model;
+    this.color = color;
+}
+
+const vehicle = new Vehicle('Tesla', 'White');
+console.log(vehicle);
+
+// Window Binding: Asignar una variable a la ventana global.
+window.color = 'Dark';
+function hello() {
+    console.log('Window Binding:', color);
+}
+
+hello();
